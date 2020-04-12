951b31a00263583ad595ea12b4e02a9f6bee2d1d
ameya <ameyaj20010@gmail.com>
2020-04-12 14:31:48 +0530

    Initial Commit

---
+++ SlideOptions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideOptions
+//
+//  Created by Ameya on 12/04/20.
+//  Copyright © 2020 Ameya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
